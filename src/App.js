c423bfbb70e35a3332303b615dc761bcfef8c22e
Marcos Carrizo <marcoscarrizo7@gmail.com>
2021-09-29 18:24:19 -0300

    actions and reducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Route } from 'react-router';
-
+import { getMovies, getSeries } from './redux/actions/actions';
+import { useDispatch } from 'react-redux';
 const data = require('./data/sample.json') 
 
 console.log('data',data)
 
 function App() {
+const dispatch = useDispatch()
+
+  useEffect(() => {
+    console.log('holi')
+  dispatch( getMovies())
+  dispatch(getSeries())
+  }, [])
+
   return (
  <React.Fragment>
 
@@ -14,3 +23,4 @@ function App() {
 }
 
 export default App;
+
--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,3 +1,19 @@
-const data = require('../../sample.json') 
+export const GET_MOVIES ='GET_MOVIES' 
+export const GET_SERIES = 'GET_SERIES'
 
-console.log('data',data)
+const data = require('../../data/sample.json')
+
+export function getMovies() {
+    return {
+            type: GET_MOVIES,
+            payload: data.entries
+        }
+}
+
+export function getSeries() {
+
+    return {
+        type: GET_SERIES,
+        payload: data.entries
+    }
+}
